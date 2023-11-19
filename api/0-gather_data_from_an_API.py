f1adc3fa63e81c9674457d773b0ffabea70e4a11
Jagger Van Winkle <JaggerVanW@gmail.com>
2023-11-18 18:33:21 -0600

    task 0

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Gathering data from API jsonplaceholder"""
+import requests
+from sys import argv
+
+
+def gather_data_api(employee_id):
+    url = 'https://jsonplaceholder.typicode.com/'
+    employee_id_url = f'{url}/users/{employee_id}'
+    todo_url = f'{url}/users/{employee_id}/todos'
+
+    user_data = requests.get(employee_id_url).json()
+    todo_data = requests.get(todo_url).json()
+
+    name = user_data.get('name')
+    task_done = 0
+
+    for task in todo_data:
+        if task['completed']:
+            task_done += 1
+
+    print(f"Employee {name} is done with tasks({task_done}/{len(todo_data)})")
+
+    for task in todo_data:
+        if task['completed']:
+            print(f"\t {task['title']}")
+
+
+if __name__ == '__main__':
+    employee_id = int(argv[1])
+    gather_data_api(employee_id)
