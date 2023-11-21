8eb7e8ca5ac5fef836390bd95f1e0af73daecb02
Jagger Van Winkle <JaggerVanW@gmail.com>
2023-11-20 19:56:39 -0600

    csv task

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Gathering data from API jsonplaceholder"""
+import csv
+import requests
+from sys import argv
+
+
+def export_to_csv(employee_id):
+    url = 'https://jsonplaceholder.typicode.com/'
+    employee_id_url = f'{url}/users/{employee_id}'
+    todo_url = f'{url}/users/{employee_id}/todos'
+
+    user_data = requests.get(employee_id_url).json()
+    todo_data = requests.get(todo_url).json()
+
+    name = user_data.get('name')
+
+    all_tasks = {
+        todo['title']: todo['completed']
+        for todo in todo_data if todo['userId'] == employee_id
+    }
+
+    with open(f"{employee_id}.csv", mode='w') as csvfile:
+        csvfile = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [
+            csvfile.writerow([employee_id, name, task_done, task])
+            for task, task_done in all_tasks.items()
+        ]
+
+if __name__ == '__main__':
+    employee_id = int(argv[1])
+    export_to_csv(employee_id)
